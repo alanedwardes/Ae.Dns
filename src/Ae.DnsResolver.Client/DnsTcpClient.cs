a15fe41b2bd9e4f354c38b5afb5988f10a90e4f5
Alan Edwardes <alan@alanedwardes.com>
2020-08-30 13:50:19 +0100

    Adds WIP DnsTcpClient implementation.

---
+++ src/Ae.DnsResolver.Client/DnsTcpClient.cs
@@ -0,0 +1,58 @@
+﻿using Ae.DnsResolver.Protocol;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Ae.DnsResolver.Client
+{
+    public sealed class DnsTcpClient : IDnsClient, IDisposable
+    {
+        private readonly ILogger<DnsTcpClient> _logger;
+        private readonly Socket _socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+        private readonly string _label;
+
+        public DnsTcpClient(ILogger<DnsTcpClient> logger, IPAddress address, string label)
+        {
+            _logger = logger;
+            _socket.Connect(address, 53);
+            _label = label;
+        }
+
+        public void Dispose()
+        {
+            try
+            {
+                _socket.Disconnect(false);
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        public async Task<byte[]> LookupRaw(DnsHeader query)
+        {
+            var raw = query.WriteDnsHeader().ToArray();
+
+            var payload = ((ushort)raw.Length).ToBytes().Concat(raw).ToArray();
+            await _socket.SendAsync(payload, SocketFlags.None);
+
+            var buffer = new byte[4096];
+            var recieve = await _socket.ReceiveAsync(buffer, SocketFlags.None);
+
+            var offset = 0;
+            var responseLength = buffer.ReadUInt16(ref offset);
+
+            var response = buffer.ReadBytes(responseLength, ref offset);
+
+            if (response.Length != responseLength)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return response;
+        }
+    }
+}
---
+++ tests/Ae.DnsResolver.Tests/Client/DnsTcpClientTests.cs
@@ -0,0 +1,27 @@
+﻿using Ae.DnsResolver.Client;
+using Ae.DnsResolver.Protocol;
+using Microsoft.Extensions.Logging.Abstractions;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Ae.DnsResolver.Tests.Client
+{
+    public class DnsTcpClientTests
+    {
+        [Fact]
+        public async Task TestLookupAlanEdwardesCom()
+        {
+            byte[] result;
+            using (var client = new DnsTcpClient(new NullLogger<DnsTcpClient>(), IPAddress.Parse("1.1.1.1"), "test"))
+            {
+                result = await client.LookupRaw(DnsHeader.CreateQuery("alanedwardes.com"));
+            }
+
+            var offset = 0;
+            var answer = result.ReadDnsAnswer(ref offset);
+
+            Assert.Equal(4, answer.Answers.Count);
+        }
+    }
+}
