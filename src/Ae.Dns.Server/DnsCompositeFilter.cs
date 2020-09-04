bbe765e3b57ac0e6d07376ed40dd8cc1cc6a9e4a
Alan Edwardes <alan@alanedwardes.com>
2020-09-04 20:08:48 +0100

    Adds more advanced filtering options.

---
+++ src/Ae.Dns.Server/DnsCompositeFilter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Ae.Dns.Protocol;
+
+namespace Ae.Dns.Server
+{
+    public sealed class DnsCompositeFilter : IDnsFilter
+    {
+        private readonly IReadOnlyCollection<IDnsFilter> _dnsFilters;
+
+        public DnsCompositeFilter(IEnumerable<IDnsFilter> dnsFilters) => _dnsFilters = dnsFilters.ToList();
+
+        public DnsCompositeFilter(params IDnsFilter[] dnsFilters) => _dnsFilters = dnsFilters;
+
+        public bool IsPermitted(DnsHeader query) => !_dnsFilters.Any() || _dnsFilters.Any(x => x.IsPermitted(query));
+    }
+}
