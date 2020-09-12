efa6697d65f4e4fe5d052ce1c9f1f458bbc001a1
Alan Edwardes <alan@alanedwardes.com>
2020-09-12 12:03:07 +0100

    Work on improving the DnsMiddleware.

--- src/Ae.Dns.Server.Http/DnsMiddleware.cs
+++ src/Ae.Dns.Server.Http/DnsMiddleware.cs
@@ -3,12 +3,15 @@ using Ae.Dns.Protocol;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Primitives;
 using System.Buffers;
+using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Ae.Dns.Server.Http
 {
     public class DnsMiddleware
     {
+        private const string DnsMessageType = "application/dns-message";
         private readonly RequestDelegate _next;
         private readonly IDnsClient _dnsClient;
 
@@ -20,6 +23,18 @@ namespace Ae.Dns.Server.Http
 
         public async Task InvokeAsync(HttpContext context)
         {
+            if (!context.Request.Path.StartsWithSegments(new PathString("/dns-query")))
+            {
+                await _next(context);
+                return;
+            }
+
+            if (!context.Request.Headers.TryGetValue("Accepts", out StringValues accepts) || !accepts.Contains(DnsMessageType))
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.UnprocessableEntity;
+                return;
+            }
+
             var ms = await context.Request.BodyReader.ReadAsync(context.RequestAborted);
 
             var buffer = ms.Buffer.ToArray();
@@ -28,7 +43,7 @@ namespace Ae.Dns.Server.Http
 
             var answer = await _dnsClient.Query(header, context.RequestAborted);
 
-            context.Response.Headers.Add("Content-Type", new StringValues("application/dns-message"));
+            context.Response.Headers.Add("Content-Type", new StringValues(DnsMessageType));
 
             await context.Response.BodyWriter.WriteAsync(answer.ToBytes(), context.RequestAborted);
         }
