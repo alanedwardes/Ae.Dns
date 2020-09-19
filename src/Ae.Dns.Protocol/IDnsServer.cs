72ec7c38e0d40fe57f8c0ab0b8215515e4b64c73
Alan Edwardes <alan@alanedwardes.com>
2020-09-19 21:53:40 +0100

    Tidies up the HTTP server project, moves IDnsServer to the protocol project.

--- src/Ae.Dns.Protocol/IDnsServer.cs
+++ src/Ae.Dns.Protocol/IDnsServer.cs
@@ -2,10 +2,10 @@
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Ae.Dns.Server
+namespace Ae.Dns.Protocol
 {
     public interface IDnsServer : IDisposable
     {
-        Task Listen(CancellationToken token);
+        Task Listen(CancellationToken token = default);
     }
 }
