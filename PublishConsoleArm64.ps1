77f23e9fccd23138f7489ec25e38375ae3b8ccea
Alan Edwardes <alan@alanedwardes.com>
2020-10-18 21:16:43 +0100

    Adds PublishConsoleLinux64.ps1

--- PublishConsoleArm64.ps1
+++ PublishConsoleArm64.ps1
@@ -4,4 +4,4 @@ Remove-Item -LiteralPath Ae.Dns.Console.zip -ErrorAction Ignore
 
 dotnet publish misc/Ae.Dns.Console --configuration Release --runtime linux-arm64
 
-Compress-Archive -Path misc/Ae.Dns.Console/bin/Release/netcoreapp3.1/linux-arm64/publish/* -DestinationPath Ae.Dns.Console.zip
+Compress-Archive -Path misc/Ae.Dns.Console/bin/Release/netcoreapp3.1/linux-arm64/publish/* -DestinationPath linux-arm64.zip
