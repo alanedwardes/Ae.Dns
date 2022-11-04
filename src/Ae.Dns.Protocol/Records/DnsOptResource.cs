b8f3d7a2b7908be327a5566b08a4ea5cef4d0586
Alan Edwardes <alan@alanedwardes.com>
2022-11-04 23:24:01 +0000

    Improves DnsMessage.ToString(), more work on DnsOptResource

--- src/Ae.Dns.Protocol/Records/DnsOptResource.cs
+++ src/Ae.Dns.Protocol/Records/DnsOptResource.cs
@@ -15,20 +15,6 @@ namespace Ae.Dns.Protocol.Records
         /// </value>
         public ReadOnlyMemory<byte> Raw { get; set; }
 
-        /// <summary>
-        /// The maximum UDP payload size a requestor can receive (without fragmentation at the IP layer).
-        /// Packets larger than this size should be truncated and the requestor should re-request via TCP or DoH.
-        /// </summary>
-        /// <value>
-        /// See https://www.rfc-editor.org/rfc/rfc6891#page-10 for more information
-        /// </value>
-        public ushort MaximumPayloadSize { get; set; }
-
-        /// <summary>
-        /// See https://www.rfc-editor.org/rfc/rfc6891#section-6.1.3 for more information.
-        /// </summary>
-        public uint Flags { get; set; }
-
         /// <inheritdoc/>
         public bool Equals(DnsOptResource other) => Raw.Span.SequenceEqual(other.Raw.Span);
 
@@ -36,20 +22,11 @@ namespace Ae.Dns.Protocol.Records
         public override bool Equals(object obj) => obj is DnsOptResource record ? Equals(record) : base.Equals(obj);
 
         /// <inheritdoc/>
-        public override int GetHashCode() => HashCode.Combine(MaximumPayloadSize, Flags, Raw);
+        public override int GetHashCode() => HashCode.Combine(Raw);
 
         /// <inheritdoc/>
         public void ReadBytes(ReadOnlyMemory<byte> bytes, ref int offset, int length)
         {
-            // This type of DNS record uses the class, ttl and rdlen data
-            // This code is not designed for that right now, so we'll just look backwards to get the data again
-            // See https://www.rfc-editor.org/rfc/rfc6891#section-6.1.2
-            var rdlenOffset = offset - sizeof(ushort);
-            var ttlOffset = rdlenOffset - sizeof(uint);
-            var classOffset = ttlOffset - sizeof(ushort);
-
-            MaximumPayloadSize = DnsByteExtensions.ReadUInt16(bytes, ref classOffset);
-            Flags = DnsByteExtensions.ReadUInt32(bytes, ref ttlOffset);
             Raw = DnsByteExtensions.ReadBytes(bytes, length, ref offset);
         }
 
@@ -60,6 +37,9 @@ namespace Ae.Dns.Protocol.Records
             Raw.CopyTo(bytes.Slice(offset));
             offset += Raw.Length;
         }
+
+        /// <inheritdoc/>
+        public override string ToString() => $"Raw: {Raw.Length} bytes";
     }
 }
 
