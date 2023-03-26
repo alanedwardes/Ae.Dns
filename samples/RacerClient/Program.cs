f923e0d282862e14cd478999a391c8c849c26439
Alan Edwardes <alan@alanedwardes.com>
2023-03-26 13:55:00 +0100

    Adds missing using statements.

--- samples/RacerClient/Program.cs
+++ samples/RacerClient/Program.cs
@@ -8,7 +8,7 @@ using IDnsClient google1 = new DnsUdpClient(IPAddress.Parse("8.8.8.8"));
 using IDnsClient google2 = new DnsUdpClient(IPAddress.Parse("8.8.4.4"));
 
 // Construct the race: you must pass at least two clients
-IDnsClient dnsClient = new DnsRacerClient(cloudFlare1, cloudFlare2, google1, google2);
+using IDnsClient dnsClient = new DnsRacerClient(cloudFlare1, cloudFlare2, google1, google2);
 
 // Race two clients in parallel, use the fatest result, discard the slowest
 DnsMessage answer = await dnsClient.Query(DnsQueryFactory.CreateQuery("google.com"));
