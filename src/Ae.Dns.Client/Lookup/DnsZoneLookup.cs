39fea182faeb6ec7c066d7e46b811807b81bdf9b
Alan Edwardes <alan@alanedwardes.com>
2024-02-05 21:46:07 +0000

    Adds DnsZoneLookup and tests.

---
+++ src/Ae.Dns.Client/Lookup/DnsZoneLookup.cs
@@ -0,0 +1,54 @@
+﻿using Ae.Dns.Client.Zone;
+using Ae.Dns.Protocol.Records;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace Ae.Dns.Client.Lookup
+{
+    /// <summary>
+    /// Allows forward and reverse address lookups using an <see cref="IDnsZone"/>.
+    /// </summary>
+    [Obsolete("Experimental: May change significantly in the future")]
+    public sealed class DnsZoneLookup : IDnsLookupSource
+    {
+        private readonly IDnsZone _dnsZone;
+
+        /// <summary>
+        /// Construct a <see cref="DnsZoneLookup"/> using the specified <see cref="IDnsZone"/>.
+        /// </summary>
+        /// <param name="dnsZone"></param>
+        public DnsZoneLookup(IDnsZone dnsZone)
+        {
+            _dnsZone = dnsZone;
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+        }
+
+        /// <inheritdoc/>
+        public bool TryForwardLookup(string hostname, out IList<IPAddress> addresses)
+        {
+            addresses = _dnsZone.Records.Where(x => x.Host == hostname)
+                .Select(x => x.Resource)
+                .OfType<DnsIpAddressResource>()
+                .Select(x => x.IPAddress)
+                .ToList();
+
+            return addresses.Count > 0;
+        }
+
+        /// <inheritdoc/>
+        public bool TryReverseLookup(IPAddress address, out IList<string> hostnames)
+        {
+            hostnames = _dnsZone.Records.Where(x => x.Resource is DnsIpAddressResource ip && ip.IPAddress == address)
+                .Select(x => x.Host.ToString())
+                .ToList();
+
+            return hostnames.Count > 0;
+        }
+    }
+}
---
+++ tests/Ae.Dns.Tests/Client/Lookup/DnsZoneLookupTests.cs
@@ -0,0 +1,67 @@
+﻿using Ae.Dns.Client.Lookup;
+using Ae.Dns.Client.Zone;
+using Ae.Dns.Protocol.Enums;
+using Ae.Dns.Protocol.Records;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+#pragma warning disable CS0618 // Type or member is obsolete
+
+namespace Ae.Dns.Tests.Client.Lookup
+{
+    public sealed class DnsZoneLookupTests
+    {
+        private sealed class DummyZoneNoRecords : IDnsZone
+        {
+            public IEnumerable<DnsResourceRecord> Records => Enumerable.Empty<DnsResourceRecord>();
+
+            public Task<bool> AddRecords(IEnumerable<DnsResourceRecord> records, CancellationToken token = default)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        [Fact]
+        public void TestLookupNoResults()
+        {
+            var lookup = new DnsZoneLookup(new DummyZoneNoRecords());
+
+            Assert.False(lookup.TryReverseLookup(IPAddress.Loopback, out var hostnames));
+            Assert.Empty(hostnames);
+            Assert.False(lookup.TryForwardLookup("wibble", out var addresses));
+            Assert.Empty(addresses);
+        }
+
+        private sealed class DummyZoneWithRecords : IDnsZone
+        {
+            public IEnumerable<DnsResourceRecord> Records => new[]
+            {
+                new DnsResourceRecord { Host = "wibble", Class = DnsQueryClass.IN, Type = DnsQueryType.A, Resource = new DnsIpAddressResource { IPAddress = IPAddress.Loopback } },
+                new DnsResourceRecord { Host = "wibble", Class = DnsQueryClass.IN, Type = DnsQueryType.A, Resource = new DnsIpAddressResource { IPAddress = IPAddress.Broadcast } },
+                new DnsResourceRecord { Host = "wibble", Class = DnsQueryClass.IN, Type = DnsQueryType.TEXT, Resource = new DnsTextResource { Entries = "hello1" } },
+                new DnsResourceRecord { Host = "wibble", Class = DnsQueryClass.IN, Type = DnsQueryType.TEXT, Resource = new DnsTextResource { Entries = "hello2" } },
+            };
+
+            public Task<bool> AddRecords(IEnumerable<DnsResourceRecord> records, CancellationToken token = default)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        [Fact]
+        public void TestLookupWithResults()
+        {
+            var lookup = new DnsZoneLookup(new DummyZoneWithRecords());
+
+            Assert.True(lookup.TryReverseLookup(IPAddress.Loopback, out var hostnames));
+            Assert.Equal(new[] { "wibble" }, hostnames);
+            Assert.True(lookup.TryForwardLookup("wibble", out var addresses));
+            Assert.Equal(new[] { IPAddress.Loopback, IPAddress.Broadcast }, addresses);
+        }
+    }
+}
